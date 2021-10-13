25011ecd60afc525c43f8908deeafdfe60e1adf4
LucasAscazuri <lucasascazuri.23@gmail.com>
2021-10-12 21:28:38 -0300

    se agrega parte del usuario

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -3,11 +3,11 @@ class UserModel{
 
     private $db;
     function __construct(){
-         $this->db = new PDO('mysql:host=localhost;'.'dbname=db_usuario;charset=utf8', 'root', '');
+         $this->db = new PDO('mysql:host=localhost;'.'dbname=usuarios;charset=utf8', 'root', '');
     }
 
     function getUser($email){
-        $sentencia = $this->db->prepare('SELECT * FROM db_usaurio WHERE email = ?');
+        $sentencia = $this->db->prepare('SELECT * FROM usaurios WHERE email = ?');
         $sentencia->execute([$email]);
         $usuario=$sentencia->fetch(PDO::FETCH_OBJ);
         return $usuario;
--- View/LoginView.php
+++ View/LoginView.php
@@ -3,6 +3,17 @@ require_once './libs/smarty-3.1.39/libs/Smarty.class.php';
 
     class LoginView{
 
+            private $smarty;
+    
+        function __construct(){
+            $this->smarty= new Smarty();
+        }
+
+        function showLogin(){
+            $this->smarty->assing('Title','Logearse');
+            $this->smarty->assing('',);
+            $this->smarty->display('Templetes/login.tpl');
+        }
 
         
     }
