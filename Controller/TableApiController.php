adb6b8c63f0819b661e68b4e5730429fe4ec0b24
LucasAscazuri <lucasascazuri.23@gmail.com>
2021-11-23 16:22:23 -0300

    si agrego api rest

--- Controller/TableApiController.php
+++ Controller/TableApiController.php
@@ -16,5 +16,42 @@ class ApiTaskController{
     }
 
 
+    public function getdata(){
+        return json_decode($this->data); 
+        }
+
+    function coment($params){
+
+        $body= $this->getData();
+
+
+        $comentario= $body->comentario;
+        $puntaje= $body->puntaje;
+        $idUser= $body->idUser;
+        $idPresupuesto= $params[':ID'];
+
+        $resultado= $this->modelComentarios->newCommentary($comentario, $puntaje, $idPresupuesto, $idUser);
+        header("Location: " . BASE_URL . 'comPresupuesto/' . $idPresupuesto); //verificar que comPresupuesto/ ande bien
+
+        if ($resultado){
+            $this->view->response("Se agregado el comentario", 200);
+        }
+        else{
+            $this->view->response("No se puedo agregar el comentario", 500);
+        }
+    }
+
+    public function deleteComent($params){
+        $id = $params[':ID'];
+        $comentario = $this->modelComentarios->getById($id);
+
+        if ($comentario) {
+            $this->modelComentarios->delete($id);
+            $this->view->response("El comentario con id {$id} se eliminó correctamente", 200);
+        }
+        else{
+            $this->view->response("No existe comentario para eliminar con id {$id}", 404);
+        }
+    }
 }
 ?>
