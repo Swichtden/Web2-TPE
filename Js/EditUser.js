e2dc26d9c02cb5db0b8e331aa1beee2d1305f6c6
Franco Perez <FrancoGabrielPerez@gmail.com>
2021-11-24 00:50:03 -0300

    movi el el form, elimine la funcion post

--- Js/EditUser.js
+++ Js/EditUser.js
@@ -9,10 +9,12 @@ function editUser(e){
 		if (userId){
 			let roleCell = document.querySelector(`#id${userId}`);
 			roleCell.innerHTML = `
-							<select type="select" name="Rol" required>
-								<option value="1">Usuarios</option>
-								<option value="2">Admin</option>
-							</select>
+							<form action="user/edit/${userId}" method="post">
+								<select type="select" name="FK_rol_id" required>
+									<option value="1">Usuarios</option>
+									<option value="2">Admin</option>
+								</select>
+							</form>
 			`;
 			let buttons = roleCell.parentElement.querySelector(".buttons");
 			buttons.innerHTML = `
@@ -36,27 +38,6 @@ function cancelEdit(e){
 	roleCell.innerHTML = `${roleCell.dataset.nombre_rol}`;
 }
 
-function save(e){
-	let userId = e.target.closest(".buttons").dataset.id_user;
-	let roleCell = document.querySelector(`#id${userId}`);
-	let newRole = roleCell.firstElementChild.value;
-	post('/user/edit/'+userId, {'FK_rol_id': newRole});
+function save(){
+	document.querySelector("form").submit();
 }
-
-function post(path, params, method='post') {
-	const form = document.createElement('form');
-	form.method = method;
-	form.action = path;
-  
-	for (const key in params) {
-		const hiddenField = document.createElement('input');
-		hiddenField.type = 'hidden';
-		hiddenField.name = key;
-		hiddenField.value = params[key];
-  
-		form.appendChild(hiddenField);
-	}
-  
-	document.body.appendChild(form);
-	form.submit();
-  }
