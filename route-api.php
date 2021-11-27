4cb530d120a9a40bcee251149c36af22e306d5e9
Franco Perez <FrancoGabrielPerez@gmail.com>
2021-11-27 20:06:11 -0300

    Actualizado con nuevos nombres de las funciones

--- route-api.php
+++ route-api.php
@@ -5,8 +5,8 @@ require_once 'Controller/CommentsApiController.php';
 $router = new Router();
 
 $router->addRoute('Comentarios/:ID_Budget', 'GET', 'CommentsApiController', 'getComments');
-$router->addRoute('Comentario', 'POST', 'CommentsApiController', 'AddComentary');
-$router->addRoute('Comentario/:ID', 'DELETE', 'CommentsApiController', 'deleteComentary');
+$router->addRoute('Comentario', 'POST', 'CommentsApiController', 'AddComment');
+$router->addRoute('Comentario/:ID', 'DELETE', 'CommentsApiController', 'deleteComment');
 
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
 
